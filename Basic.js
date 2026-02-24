7aa96bd71c490e4d92cdfc3ec0209f35f09c1c8a
Aditya Ubale <ubaleaditya96@gmail.com>
2026-02-24 20:08:26 +0530

    Add files via upload

---
+++ Basic.js
@@ -0,0 +1,20 @@
+/*console.log("Hello World")
+
+
+let fruit = "Apple";
+console.log(fruit);         
+
+let a=10;
+let b=20;
+console.log(a-b);*/
+
+//let Name = "Aditya";
+// let age = 21;
+
+// console.log(`My Name is ${Name} and My Age is ${age}`)
+
+let a;
+console.log(a);
+
+let b=null;
+console.log(b);
