ce6fc9bb17e681f50d2c344044f43069679c3209
Aditya Ubale <ubaleaditya96@gmail.com>
2026-02-26 18:47:31 +0530

    Add files via upload

--- Basic.js
+++ Basic.js
@@ -1,20 +1,120 @@
-/*console.log("Hello World")
+// /*console.log("Hello World")
 
 
-let fruit = "Apple";
-console.log(fruit);         
+// let fruit = "Apple";
+// console.log(fruit);         
 
-let a=10;
-let b=20;
-console.log(a-b);*/
+// let a=10;
+// let b=20;
+// console.log(a-b);*/
 
-//let Name = "Aditya";
-// let age = 21;
+// //let Name = "Aditya";
+// // let age = 21;
 
-// console.log(`My Name is ${Name} and My Age is ${age}`)
+// // console.log(`My Name is ${Name} and My Age is ${age}`)
 
-let a;
-console.log(a);
+// let a;
+// console.log(a);
 
-let b=null;
-console.log(b);
+// let b=null;
+// console.log(b);
+
+
+/*.........................................................................................*/
+                                 // Arithmatic operators
+
+// let a = 10;
+// let b = 2;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+// Urinary Operator
+ 
+// let x =1
+// x = -x;
+// console.log(x);
+
+// Exponential **
+// Means 2**3. like that 
+
+// let a = 2 ;
+// let b = 4;
+// console.log(a**b);
+
+
+// let a = 5 + 10 * 2 / 5 - 3;
+// console.log(a);
+
+// let Name = "Aditya";
+// let Age = 21;
+// console.log("My name is "+Name+" My Age is "+Age);
+
+
+//.           + Operator Behaviour          
+
+// console.log("2"+5);
+
+// it will convert 5 into string and then concat them
+
+// console.log("2" - 5);
+
+// it will convert 2 into the number and then perform mathematical operation
+
+// console.log( 6 - "abc");
+
+// it will print NaN beacouse the operation cant be performed
+
+
+// convert string to number
+// let a = "4";
+// let b = "3";
+// console.log(Number(a) + Number(b));
+// console.log(+a + +b);
+
+
+// let a = 1;
+// let b = 2;
+
+// let c = 3-(a=b+1);
+//  console.log(a);
+//  console.log(c);
+
+// Modify in place
+
+// let a=5;
+// a+=2;
+// a+=5;
+// console.log(a);
+
+//INCREMENT AND DECREMENT OPERATOR
+
+// let a = 5;
+// let b = a++;
+// console.log(b);
+
+// let b = ++a;
+// console.log(b);
+
+// let totalPrice = 700;
+// let discount = 15;
+// let finalPrice = totalPrice-(totalPrice*discount)/100;
+// console.log(finalPrice);
+
+// Feranight to celcius
+
+// let feranite=100;
+// let celcius=(feranite-32)*5/9;
+// console.log(celcius);
+
+// Salary Question
+
+// let previousSalary = 50000;
+// let i = 15;
+// let d = 10;
+// let finalSalary = previousSalary+(previousSalary*i/100)-(previousSalary*d/100);
+// console.log(finalSalary);
+
