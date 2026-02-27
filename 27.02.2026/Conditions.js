c536930d0212ac3230e7c71901b35711a08dce9f
Aditya Ubale <ubaleaditya96@gmail.com>
2026-02-27 14:38:42 +0530

    Add files via upload

---
+++ 27.02.2026/Conditions.js
@@ -0,0 +1,36 @@
+// COMPARISON OPERATORS. 
+
+// Less than and greater than
+
+// console.log(2 > 3); // False 
+// console.log(3 > 2); // True
+
+// less than equal to and greater than equal to 
+
+// console.log(2>=2);  // true
+
+// = , == , === Operator
+
+// console.log(3 == 4);    // False
+// console.log(3 == 3);    // true
+// console.log(3 == "3");      // true
+// console.log(3 === "3");     // False
+
+// != , !== Operator
+
+// console.log(3 != 5); // true
+// console.log(5 != 5);    // false
+// console.log(5 != "5");  // false
+// console.log(5 !== "5"); // true
+
+// String Comparison
+
+// console.log("Z" > "A");     //true
+// console.log("Glow" > "Glee");   // true
+// console.log("Aditya" > "Ashish");   // false
+
+// COMPARISON IN DIFFERENT TYPES
+// When comparing 2 values if any one value is a number JS will be convert the other to a number and then the comaprison is done between two numbers
+console.log("2" > 4); // flase
+console.log(true == 1); // true
+console.log(false == 0);
---
+++ 27.02.2026/MathFunctions.js
@@ -0,0 +1,41 @@
+// Math round
+
+// console.log(Math.round(4.6));    //5
+// console.log(Math.round(4.3));    //4
+// console.log(Math.round(-4.6));   //-5
+// console.log(Math.round(-4.3));   //-4
+
+// Math Floor
+
+// console.log(Math.floor(4.3));   //4
+// console.log(Math.floor(4.9));   //4
+// console.log(Math.floor(-4.6));   //-5
+// console.log(Math.floor(-4.3));   //-5
+
+// Math Ceil
+
+// console.log(Math.ceil(4.6)); //5
+// console.log(Math.ceil(4.3)); //5
+// console.log(Math.ceil(-4.6));    //-4
+// console.log(Math.ceil(-4.3));    //-4
+
+// Math toFixed
+
+// let a=23.89077;
+// console.log(a.toFixed(0));  //24
+// console.log(a.toFixed(1));  //23.9
+// console.log(a.toFixed(2));  //23.89
+// console.log(a.toFixed(3));  //23.891
+// console.log(a.toFixed(4));  //23.8908
+
+// parseInt
+
+// console.log("46");  //46
+// console.log(parseInt("44"));    //44
+// console.log(parseInt("66.75")); //66
+// console.log(parseInt("44px"));  44
+
+//parseFloat
+
+console.log(parseFloat("34"));
+console.log(parseFloat("44.567"));
