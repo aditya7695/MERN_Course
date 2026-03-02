3637cd1b482f8c75789822ee149c3b5ffad3d620
Aditya Ubale <ubaleaditya96@gmail.com>
2026-03-02 18:53:05 +0530

    Add files via upload

---
+++ 02.03.2026/Conditions.js
@@ -0,0 +1,70 @@
+//..............................IF Statement.............................
+
+// let age=21;
+// if(age>=21){
+//     console.log("You can vote");
+// }
+
+// this is alsoo another way to write the if condition but it is not re
+// if(age>=18)console.log("You Can vote");
+
+//..............................Else Statement..........................
+
+// let age = 15
+// if(age>=18){
+
+//     console.log("You Can Vote");
+// }
+// else{
+
+//     console.log("You Cannot vote");
+// }
+
+//..............................Else if  Statement..........................
+
+// let year=1980;
+// if(year>1947){
+//     console.log("India got indepandance earlier");
+// }
+// else if(year<1947){
+//     console.log("India didnt got the independance yet");
+// }
+// else{
+//     console.log("Your answer is right");
+// }
+
+
+//................................Nested if statement.........................
+
+// let age;
+// if(age){
+//     console.log("You have entered valid value");
+//     if(age>=18){
+//         console.log("You can vote");
+//     }else{
+//         console.log("You cannot vote");
+//     }
+// }else{
+//     console.log("You have entered invalid value");
+// }
+
+
+//.................................Ternary Operator.............................
+
+// let age=2;
+
+// age>=18 ? console.log("You can vote") : console.log("You cannot vote");
+
+// let message = (age>18)?"you can vote":"you cant vote";
+
+// console.log(message);
+
+// nested if in ternary operator
+
+// let age;
+
+// let message=(age) ? (age>=18) ? "you can vote" : "you cannot vote " : "not valid input";console.log(message);
+
+let score = 80;
+let message = score>=90 ? "Grade A" : score>=80 ? "Grade B" : score>=70 ? "Grade C" : score>=60 ? "Grade D" : "Grade F";
+console.log(message);
