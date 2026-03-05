81a5b9c564d85f8d3ee79553fdcff7263442c8bb
Aditya Ubale <ubaleaditya96@gmail.com>
2026-03-05 12:58:59 +0530

    Add files via upload

---
+++ 05.03.2026/Ques.js
@@ -0,0 +1,37 @@
+// Question 1. 
+// let n=1;
+// if(n>0){
+//     console.log("It is Positive");
+// }else if(n==0){
+//     console.log("It is Zero");
+// }
+// else{
+//     console.log("It is negative");
+// }
+
+// Question  2.       
+
+// let n=12.89;
+// let integerpart=Math.floor(n);
+// if (integerpart%2==0){
+//     console.log("Number is Even");
+// }else{
+//     console.log("Number is odd");
+// }
+
+// Question 3.  
+
+let a=23;
+let b=399;
+let c=577;
+if (a>b){
+    if(a>c)
+    {  console.log("a is greatest"); }
+}else if (b>c){
+    console.log("b is greatest");
+    }
+else if(c>a){
+    if(c>b){
+        console.log("c is greatest");
+    }
+}
---
+++ 05.03.2026/logical.js
@@ -0,0 +1,79 @@
+//.........................OR Operator....................................................
+
+// console.log("Logical Operators");
+//  console.log(true||true);
+//  console.log(true||false);
+//  console.log(false||true);
+//  console.log(false||false);
+
+// let a=0;
+// let b=0;
+// let c=false;
+
+// let result=console.log(a||b||c);
+
+// Questions.  
+
+// let firstName ="";
+// let lastName="";
+
+// let result = firstName || lastName || "Anonymus" ;
+
+// console.log(result);
+
+// true||console.log("Hello");
+// false||console.log("Hello2");
+
+//.........................OR Operator....................................................
+
+//.........................AND Operator....................................................
+
+// console.log(true&&true);
+// console.log(true&&false);
+// console.log(false&&true);
+// console.log(false&&false);
+
+// let age=20;
+// let isMale=true;
+
+// if (age>18 && isMale){
+//     console.log("Its an adult male");
+// }
+
+// let a=23;
+// let b=399;
+// let c=577;
+
+// if (a>b){
+//     if(a>c)
+//     {  console.log("a is greatest"); }
+// }else if (b>c){
+//     console.log("b is greatest");
+//     }
+// else if(c>a){
+//     if(c>b){
+//         console.log("c is greatest");
+//     }
+// }
+
+// if(a>b&&a>c){
+//     console.log("a is greatest");
+// }else if (b>c){
+//     console.log("b is greatest");
+// }
+// else if(c>a&&c>b){
+//     console.log("c is greatest");
+// }
+
+
+//.........................AND Operator....................................................
+
+//.........................NOT Operator....................................................
+// let isFemale=true;
+// isFemale=!isFemale;
+// console.log(isFemale);
+
+// console.log(!true);
+// console.log(!0);
+// console.log(!3);
+
