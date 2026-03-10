e91ef90647b7026713e4e2d1e25d8c5e93c4bf32
Aditya Ubale <ubaleaditya96@gmail.com>
2026-03-10 22:57:42 +0530

    Add files via upload

---
+++ 10.03.2026/Functions.js
@@ -0,0 +1,33 @@
+
+// let lenght1=30;
+// let width1=34;
+// function area(length,width){
+//     console.log(length*width);
+// }
+// area(lenght1,width1);
+
+
+// let a=67;
+// let b=56;
+// function Addition(a,b){
+//     console.log(a+b);
+// }
+// function Substraction(a,b){
+//     console.log(a-b);
+// }
+// function Multiplication(a,b){
+//     console.log(a*b);
+// }
+// function Division(a,b){
+//     console.log(a/b);
+// }
+// Addition(a,b);
+// Substraction(a,b);
+// Multiplication(a,b);
+// Division(a,b);
+
+
+
+
+
+
