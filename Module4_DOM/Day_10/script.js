bf6b7444eb0dad696c02531e1ac9a395703c13d5
aditya7695 <ubaleaditya96@gmail.com>
2026-06-22 11:30:41 +0530

    All Files Upload

--- Module4_DOM/Day_10/script.js
+++ Module4_DOM/Day_10/script.js
@@ -1,2 +1,158 @@
 // Encapsulation is hiding internal implementation details and  exposing only necessary interface .
-// private feilds
+// private feilds and public feilds
+
+// class DataComparison{
+//     publicData = "I am public data";
+//     #privateData = "I am private data";
+
+//     publicMethod(){
+//         console.log("public mrthod called");
+//         this.#privateMethod()
+        
+//     }
+//     #privateMethod(){
+//         console.log("private method called");
+//         console.log(this.#privateData);
+//     }
+//     textAccess(){
+//         console.log(this.publicData);
+//         console.log(this.#privateData);
+//         this.#privateMethod();
+//         this.publicMethod();
+//     }
+// }
+
+// const obj = new DataComparison();
+// // console.log(obj.publicData);
+// // obj.publicMethod();
+// obj.textAccess()
+
+
+// DOM in classes
+
+// class Card {
+//     constructor(title,content){
+//         this.title = title;
+//         this.content = content;
+//         this.element = null;
+//     }
+//     render(parentId){
+//         this.element = document.createElement('div');
+//         this.element.className = 'card';
+//         this.element.innerHTML = `<h3>${this.title}</h3> <p>${this.content}</p>`;
+//         const parent = document.getElementById(parentId);
+//         parent.appendChild(this.element)
+//     }
+//     remove(){
+//         this.element.remove()
+//     }
+// }
+
+// const card1 = new Card("Title 1","Content 1 sjkdafgbdjdhb kdashjbxmbh ioh sdluku hkjhg vkzdhf jkhsd vs fvdjshkv f jhkfsdbmn")
+// card1.render("container")
+
+
+// class inheritance
+// parent class
+
+// class Animal{
+//     constructor(name){
+//         this.name = name;
+//     }
+//     speak(){
+//         console.log(`${this.name} Makes a sound `);
+//     }
+// }
+
+// class Dog extends Animal{
+
+//     bark(){
+//         console.log(`${this.name} barks`);
+//     }
+// }
+
+// const dog = new Dog("Buddy")
+// dog.bark()
+
+// super() : the super calls keyword calls the parent 
+// constructor and most he called before 
+// accesing this in child class
+
+
+// class Person{
+//     constructor(name,age){
+//         this.name = name
+//         this.age = age
+//     }
+
+// }
+
+// class Student extends Person{
+//     constructor(name,age,gender){
+//         super(name,age)
+//         this.gender = gender;
+//     }
+
+//     study(){
+//         console.log(`${this.name} Having age ${this.age} and Gender ${this.gender} is Studying`);
+//     }
+// }
+
+// const student1 = new Student("Aditya",21,"Male")
+// student1.study()
+
+
+// accessing parent class methods in the child class
+
+// class Phone{
+//     constructor(brand) {
+//         this.brand = brand
+//     }
+//     call(){
+//         console.log(`${this.brand} calling you`);
+//     }
+// }
+
+// class SmartPhone extends Phone{
+//     constructor(brand){
+//         super(brand);
+//     }
+//     photo(){
+//         console.log(`${this.brand} take photo`);
+//     }
+// }
+
+// const phone1 = new SmartPhone("Apple");
+// phone1.photo()
+// phone1.call()
+
+
+class Phone{
+    #price
+    constructor(brand,price) {
+        this.brand = brand
+        this.#price = price;
+    }
+    call(){
+        console.log(`${this.brand} calling you`);
+    }
+    getPrice(){
+        return this.#price
+    }
+}
+
+class SmartPhone extends Phone{
+    constructor(brand,price){
+        super(brand,price);
+    }
+    photo(){
+        console.log(`${this.brand} take photo`);
+        console.log(`price of the phone is ${this.getPrice()}`);
+    }
+}
+
+const phone1 = new SmartPhone("Apple",50000);
+phone1.photo()
+phone1.call()
+
+
