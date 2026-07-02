8ec018007a4addda53f7936ce3ab4b8fbe1a4ae7
aditya7695 <ubaleaditya96@gmail.com>
2026-07-02 10:01:20 +0530

    Debouncing and throttling

---
+++ Module5_Async/Day_5_DebouncingAndThrottling/script.js
@@ -0,0 +1,65 @@
+// const debounce = (fn,delay) =>{
+//     let timerId ; 
+//     return function(...args){
+//         clearTimeout(timerId);
+//         timerId = setTimeout(()=>{
+//             fn(...args)
+//         },delay)
+//     }
+// }
+
+// const search = (query) =>{
+//     document.getElementById('result').textContent="Searching for : "+query
+//     console.log("Searching for : "+query);
+    
+// }
+
+// const searchWithDebounce = debounce(search,400);
+
+// const input = document.getElementById('searchInput');
+// input.addEventListener('input',(e)=>{
+//     searchWithDebounce(e.target.value)
+// })
+
+
+
+// Throttling.        
+
+
+// const throttle = (fn,delay) =>{
+//     let lastCall = 0;
+//     return function(...args){
+//         const now = Date.now();
+//         if(now-lastCall < delay){
+//             return
+//         }
+//         lastCall = now
+//         return fn(...args)
+//     }
+// }
+
+// const burstShoot = () =>{
+//     console.log("Shoot");
+// }
+
+// const shootWithThrottle = throttle(burstShoot,1000)
+
+// const shoot = document.getElementById('shoot');
+// shoot.addEventListener('click',(e)=>{
+//     shootWithThrottle();
+// })
+
+ 
+async function fetchData(value){
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users?name_like=${value}`);
+    const data = await res.json();
+    data.map(item =>{
+        console.log(item.name);
+    })
+   
+}
+const input = document.getElementById('searchInput')
+input.addEventListener('input',(e)=>{
+    fetchData(e.target.value);
+})
+
---
+++ Module5_Async/Day_5_PromiseAPI/Script.js
@@ -0,0 +1,109 @@
+// async function testing(){
+//     try{
+//         let res = await Promise.all([
+//             fetch("https://api.github.com/users/iliakan"),
+//             fetch("https://api.github.com/users/remy"),
+//             fetch("https://api.github.com/users/jeresig")
+//         ])
+//         // res.map(async(item)=>{
+//         //     let data = await item.json();
+//         //     console.log(data);
+//         // })
+//         const dataArray = await Promise.all(res.map(item =>item.json()));
+//         console.log(dataArray.map(item => item));
+//     }catch(err){
+//         console.log(err);
+//     }
+// }
+// testing();
+
+// Promise.allSettled([
+//     new Promise((res,rej)=>{
+//         setTimeout(() => {
+//             rej("A")
+//         }, 3000);
+//     }),
+//     new Promise((res,rej)=>{
+//         setTimeout(() => {
+//             res("B")
+//         }, 2000);
+//     }),
+//     new Promise((res,rej)=>{
+//         setTimeout(() => {
+//             res("C")
+//         }, 1000);
+//     })
+// ]).then((item)=>console.log(item))
+// .catch((err)=>console.log(err))
+
+// Promise.allSettled just waits for all promises  to settle
+// regardless of its result(unlike Promise.all which 
+// stops as soon as a promise is rejected)
+
+// Promise.race([
+//     new Promise((res,rej)=>{
+//         setTimeout(() => {
+//             res("A")
+//         }, 3000);
+//     }),
+//     new Promise((res,rej)=>{
+//         setTimeout(() => {
+//             rej("B")
+//         }, 200);
+//     }),
+//     new Promise((res,rej)=>{
+//         setTimeout(() => {
+//             rej("C")
+//         }, 1000);
+//     })
+// ]).then((item)=>console.log(item))
+// .catch((err)=>console.log(err))
+
+// B
+
+// Promise.race give me the first promise that 
+// Settels (i.e it does not care if the promise 
+// fullfilled or rejected)
+
+
+// Promise.any([
+//     new Promise((res,rej)=>{
+//         setTimeout(() => {
+//             res("A")
+//         }, 3000);
+//     }),
+//     new Promise((res,rej)=>{
+//         setTimeout(() => {
+//             rej("B")
+//         }, 200);
+//     }),
+//     new Promise((res,rej)=>{
+//         setTimeout(() => {
+//             res("C")
+//         }, 1000);
+//     })
+// ]).then((item)=>console.log(item))
+// .catch((err)=>console.log(err))
+
+// C
+
+// Promise.any give me the result of 1st promise 
+// that resolves or fullfills
+
+async function testing(){
+    try{
+        let res = await Promise.any([
+            fetch("https://jsonplaceholder.typicode.com/posts"),
+            fetch("https://jsonplaceholder.typicode.com/comments"),
+            fetch("https://jsonplaceholder.typicode.com/users")
+        ])
+        const data = await res.json();
+        console.log(data);
+    }catch(err){
+        console.log(err);
+    }
+}
+testing();
+
+
+
