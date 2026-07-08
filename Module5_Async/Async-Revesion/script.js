1db9cd32e2b1feb7346c4fe3b7ac7dc85e3e6c34
aditya7695 <ubaleaditya96@gmail.com>
2026-07-08 16:32:05 +0530

    8-07-2026 Push

---
+++ Module5_Async/Async-Revesion/script.js
@@ -0,0 +1,16 @@
+//  npm -> node package manager
+//  it sused to install and organize dependencies 
+
+//  npx -> node package executer
+//  it directly executes the packages command w/o permenently installing it 
+
+// Main difference
+
+// ------------------------------------------------------------------------------------------------
+// aspect                   npm                                     npx                           -  
+// ------------------------------------------------------------------------------------------------
+// Primary purpose        -  install and manage packages         -    run package executables     -
+// stores packages        -  yes usually in node_modules         -    not necessarily             -
+// update package.json    -  Often yes                           -    NO                          -
+// Best for               -  Long term project dependencies      -    one-time commands           -
+// ------------------------------------------------------------------------------------------------
