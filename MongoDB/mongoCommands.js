be30dd5ab816bad2a686ea23b3d953779b16541e
aditya7695 <ubaleaditya96@gmail.com>
2026-08-13 12:26:44 +0530

    MongoDB

---
+++ MongoDB/mongoCommands.js
@@ -0,0 +1,56 @@
+db.orders.insertMany([
+  {
+      _id: 1,
+      item: "almonds",
+      price: 12,
+      quantity: 2
+    },
+    {
+      _id: 2,
+      item: "pecans",
+      price: 20,
+      quantity: 1
+    },
+    {
+      _id: 3
+    }
+]);
+
+db.inventory.insertMany([
+{
+      _id: 1,
+      sku: "almonds",
+      description: "product 1",
+      instock: 120
+    },
+    {
+      _id: 2,
+      sku: "bread",
+      description: "product 2",
+      instock: 80
+    },
+    {
+      _id: 3,
+      sku: "cashews",
+      description: "product 3",
+      instock: 60
+    },
+    {
+      _id: 4,
+      sku: "pecans",
+      description: "product 4",
+      instock: 70
+    },
+    {
+      _id: 5,
+      sku: null,
+      description: "Incomplete"
+    }
+]);
+
+// reading your document
+db.orders.find()
+db.inventory.find()
+
+// findOne -> will return you a single object
+db.inventory.findOne()
