cc276ff1295ffe7e8d7d72c7b6341e574bc22557
aditya7695 <ubaleaditya96@gmail.com>
2026-08-27 11:03:27 +0530

    Lazy and suspense

--- Module6_React/Day_19_frowardRef/src/App.jsx
+++ Module6_React/Day_19_frowardRef/src/App.jsx
@@ -1,10 +1,21 @@
-import { useRef } from "react"
-import Child from "./Child"
+import { lazy, Suspense, useRef, useState } from "react"
+const Child = lazy(()=> {
+  
+  const result = new Promise(
+    (resolve) => {
+      setTimeout(()=>{
+        console.log("We are rendering the Child component")
+        resolve(import ('./Child'))
+      },5000)
+  })
+  return result;
+  })
 
 
 const App = () => {
   const iRef = useRef(null);
   const hRef = useRef(null);
+  const [show , setShow] = useState(false)
 
   const finalRef = {
     iRef : iRef,
@@ -13,15 +24,24 @@ const App = () => {
   return (
     <div>
       <h1>Learning Forward ref</h1>
-      <Child 
+      {
+        show && 
+       (< Suspense fallback = {<p>Loading child component ... </p>} >
+
+          <Child 
       name = {"Aditya"}
       ref = {finalRef}
       />
+       </Suspense>
+       )
+      }
+    
       <button 
        onClick={()=>{
-        iRef.current.value = "This is the text input";
-        iRef.current.focus();
-        hRef.current.style.color = 'red';
+        // iRef.current.value = "This is the text input";
+        // iRef.current.focus();
+        // hRef.current.style.color = 'red';
+        setShow(!show);
        }} >Click</button>
     </div>
   )
